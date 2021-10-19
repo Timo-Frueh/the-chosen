91b6fd006b63a9da079ef7af3225442c35d897be
Timo Früh <timo.frueh@icloud.com>
2021-10-19 15:12:29 +0200

    wrote rpginfo class

---
+++ src/rpginfo.py
@@ -0,0 +1,16 @@
+class RPGInfo:
+    author = None
+    title = None
+    subtitle = None
+    welcome_message = None
+
+    @classmethod
+    def welcome(cls):
+        print(f"Welcome to {cls.title}: {cls.subtitle}")
+        print(cls.welcome_message)
+
+    @classmethod
+    def credits(cls):
+        print(f"This was {cls.title}")
+        print("Thank you for playing!")
+        print(f"\nMade by {cls.author}")
