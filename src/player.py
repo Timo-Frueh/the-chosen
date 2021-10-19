ebf61c9587b1f4217ac46d979db96d1b57713926
Timo Früh <timo.frueh@icloud.com>
2021-10-19 14:24:57 +0200

    reorder the player class

--- src/player.py
+++ src/player.py
@@ -4,15 +4,15 @@ class Player:
         self.inventory = []
         self.current_room = starting_room
 
-    def get_name(self):
-        return self.name
-
-    def get_current_room(self):
-        return self.current_room
-
     def move(self, direction):
         if direction in self.current_room.get_linked_rooms():
             self.current_room = self.current_room.get_linked_rooms()[direction]
         else:
             print("You can't go that way")
             print("")
+
+    def get_name(self):
+        return self.name
+
+    def get_current_room(self):
+        return self.current_room
