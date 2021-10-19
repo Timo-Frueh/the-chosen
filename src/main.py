7d75b33cd8b051340f4ce53b88db7faf470b25b4
Timo Früh <timo.frueh@icloud.com>
2021-10-19 14:19:40 +0200

    wrote main class and gameloop with two testrooms

---
+++ src/main.py
@@ -0,0 +1,50 @@
+from room import Room
+from player import Player
+
+
+class Main:
+    def __init__(self):
+
+        self.testroom = Room(room_name="Testroom")
+        self.testroom.set_desc("This is a testroom")
+
+        self.testroom2 = Room(room_name="Testroom 2")
+        self.testroom2.set_desc("This is another testroom")
+
+        self.testroom.link_room("south", self.testroom2)
+        self.testroom2.link_room("north", self.testroom)
+
+        self.player_name = input("Who are you? ")
+        self.player = Player(player_name=self.player_name, starting_room=self.testroom)
+
+        print("")
+
+        self.player.get_current_room().describe()
+
+    def mainloop(self):
+        dead = False
+        victory = False
+        while not dead or victory:
+            print("")
+            command = input("> ").lower().replace(" ", "")
+
+            if command in ["north", "east", "south", "west"]:
+                self.player.move(command)
+                self.player.get_current_room().describe()
+
+            elif command in ["quit", "exit"]:
+                confirm = input("Do you really want to quit? [y|n] ")
+                if confirm == "y":
+                    dead = True
+
+            elif command in ["look", "l"]:
+                self.player.get_current_room().describe()
+
+            else:
+                print(f"I do not know what you mean by {command}")
+
+
+if __name__ == "__main__":
+    Game = Main()
+
+    Game.mainloop()
