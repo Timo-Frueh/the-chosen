5586647691ec41fd211f5cfca7c1563ecbc85d92
Timo Früh <timo.frueh@icloud.com>
2021-10-20 14:52:35 +0200

    improve door displaying

--- src/room.py
+++ src/room.py
@@ -27,8 +27,27 @@ class Room:
             else:
                 item.describe()
 
-        for direction in self.linked_rooms:
-            print(f"There is a door to the {direction}.")
+        self.print_doors()
+
+    def print_doors(self):
+        if len(self.linked_rooms) == 1:
+            for direction in self.linked_rooms:
+                print(f"There is a door to the {direction}.")
+
+        elif len(self.linked_rooms) == 2:
+            directions = []
+            for direction in self.linked_rooms:
+                directions.append(direction)
+            print(f"There are doors to the {directions[0]} and {directions[1]}.")
+
+        elif len(self.linked_rooms) == 3:
+            directions = []
+            for direction in self.linked_rooms:
+                directions.append(direction)
+            print(f"There are doors to the {directions[0]}, {directions[1]} and {directions[2]}.")
+
+        elif len(self.linked_rooms) == 4:
+            print("There are doors to all directions.")
 
     def get_desc(self):
         return self.description
