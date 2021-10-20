6114cbdc0dc480c2579eb67925a0f4dfba71837e
Timo Früh <timo.frueh@icloud.com>
2021-10-20 12:31:33 +0200

    implement description files

--- src/main.py
+++ src/main.py
@@ -15,55 +15,65 @@ class Main:
         clear()
 
         self.file_path = os.path.dirname(os.path.abspath(__file__))
-        self.welcome_file = open(os.path.join(self.file_path, "resources", "welcome_message.txt"), "r")
-        self.cellar_desc_file = open(os.path.join(self.file_path, "resources", "cellar_description.txt"), "r")
+        self.welcome_f = open(os.path.join(self.file_path, "resources", "welcome_message.txt"), "r")
+        self.cellar_f = open(os.path.join(self.file_path, "resources", "cellar.txt"), "r")
+        self.stairwell_f = open(os.path.join(self.file_path, "resources", "stairwell.txt"), "r")
+        self.hall_f = open(os.path.join(self.file_path, "resources", "hall.txt"), "r")
+        self.west_room_f = open(os.path.join(self.file_path, "resources", "west_room.txt"), "r")
+        self.trophy_room_f = open(os.path.join(self.file_path, "resources", "trophy_room.txt"), "r")
+        self.ns_passageway_f = open(os.path.join(self.file_path, "resources", "ns_passageway.txt"), "r")
+        self.staff_room_f = open(os.path.join(self.file_path, "resources", "staff_room.txt"), "r")
+        self.library_f = open(os.path.join(self.file_path, "resources", "library.txt"), "r")
+        self.library_entrance_f = open(os.path.join(self.file_path, "resources", "library_entrance.txt"), "r")
+        self.east_room_f = open(os.path.join(self.file_path, "resources", "east_room.txt"), "r")
+        self.throne_entrance_f = open(os.path.join(self.file_path, "resources", "throne_entrance.txt"), "r")
+        self.hidden_room_f = open(os.path.join(self.file_path, "resources", "hidden_room.txt"), "r")
+        self.throne_room_f = open(os.path.join(self.file_path, "resources", "throne_room.txt"), "r")
 
         RPGInfo.author = "Timo Früh"
         RPGInfo.title = "The Chosen"
         RPGInfo.subtitle = "At Nights End"
 
-        RPGInfo.welcome_message = self.welcome_file.read()
-
-        RPGInfo.welcome()
+        RPGInfo.welcome_message = self.welcome_f.read()
 
         self.cellar = Room(room_name="Cellar")
-        self.cellar.set_desc(self.cellar_desc_file.read())
+        self.cellar.set_desc(self.cellar_f.read())
 
         self.stairwell = Room(room_name="Stairwell to the Cellar")
-        self.stairwell.set_desc("Stairwell description")
+        self.stairwell.set_desc(self.stairwell_f.read())
 
         self.hall = Room(room_name="The Hall")
-        self.hall.set_desc("Hall description")
+        self.hall.set_desc(self.hall_f.read())
 
         self.west_room = Room(room_name="Room West to the Hall")
-        self.west_room.set_desc("West Room description")
+        self.west_room.set_desc(self.west_room_f.read())
 
         self.trophy_room = Room(room_name="Trophy Room")
-        self.trophy_room.set_desc("Trophy Room description")
+        self.trophy_room.set_desc(self.trophy_room_f.read())
 
         self.ns_passageway = Room(room_name="N/S Passageway")
-        self.ns_passageway.set_desc("N/S Passageway description")
+        self.ns_passageway.set_desc(self.ns_passageway_f.read())
 
         self.staff_room = Room(room_name="Library Staff Room")
-        self.staff_room.set_desc("Staff Room description")
+        self.staff_room.set_desc(self.staff_room_f.read())
 
         self.library = Room(room_name="The Library")
-        self.library.set_desc("Library description")
+        self.library.set_desc(self.library_f.read())
 
         self.library_entrance = Room(room_name="Library Entrance")
-        self.library_entrance.set_desc("Library Entrance description")
+        self.library_entrance.set_desc(self.library_entrance_f.read())
 
         self.east_room = Room(room_name="Room East to the Hall")
-        self.east_room.set_desc("East Room description")
+        self.east_room.set_desc(self.east_room_f.read())
 
         self.throne_entrance = Room(room_name="Entrance to the Throne Room")
-        self.throne_entrance.set_desc("Throne Room Entrance description")
+        self.throne_entrance.set_desc(self.throne_entrance_f.read())
 
         self.hidden_room = Room(room_name="Hidden Room")
-        self.hidden_room.set_desc("Hidden Room description")
+        self.hidden_room.set_desc(self.hidden_room_f.read())
 
         self.throne_room = Room(room_name="The Throne Room")
-        self.throne_room.set_desc("Throne Room description")
+        self.throne_room.set_desc(self.throne_room_f.read())
 
         self.cellar.link_room(direction="west", room=self.stairwell)
         self.stairwell.link_room(direction="east", room=self.cellar)
@@ -98,14 +108,16 @@ class Main:
         self.throne_entrance.link_room(direction="north", room=self.throne_room)
         self.throne_room.link_room(direction="south", room=self.throne_entrance)
 
+        self.longsword = Item(item_name="sword")
+        self.longsword.set_description("a typical European longsword of a length a bit longer than your arm")
+        self.cellar.add_item(self.longsword)
+
+        RPGInfo.welcome()
+
         self.player_name = input("What is your name? ")
         if self.player_name.replace(" ", "") == "":
             self.player_name = "Stranger"
         self.player = Player(player_name=self.player_name, starting_room=self.cellar)
-
-        self.longsword = Item(item_name="sword")
-        self.longsword.set_description("a typical European longsword of a length a bit longer than your arm")
-        self.cellar.add_item(self.longsword)
 
         print("\nYou look around.")
 
