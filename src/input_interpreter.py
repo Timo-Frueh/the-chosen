1d50242ddfbde8f277c7975c8f21d8c238d543ee
Timo Früh <timo.frueh@icloud.com>
2021-10-22 13:01:19 +0200

    write InputInterpreter class

--- src/input_interpreter.py
+++ src/input_interpreter.py
@@ -1,7 +1,19 @@
 class InputInterpreter:
 
-    def interpret_single(self, command):
-        pass
+    @staticmethod
+    def interpret_single(command, key):
+        output = command.replace(key, "")
 
-    def interpret_double(self, command, separator):
-        pass
+        return output
+
+    @staticmethod
+    def interpret_double(command, key, separator):
+        without_key = command.replace(key, "")
+        output = without_key.split(separator)
+
+        try:
+            output[1]
+        except IndexError:
+            output.append(None)
+
+        return output
