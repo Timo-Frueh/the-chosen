df1e80f51938a8db11d137a76b71b1455fa4cfaa
Timo Früh <timo.frueh@icloud.com>
2021-10-25 20:19:09 +0200

    add commands to main.py

--- the_chosen/main.py
+++ the_chosen/main.py
@@ -13,8 +13,10 @@ from clear_screen import clear
 class Main:
     def __init__(self):
 
+        # clear the screen at the beginning of the game
         clear()
 
+        # load all room description files and the welcome message file
         self.file_path = os.path.dirname(os.path.abspath(__file__))
         self.welcome_f = open(os.path.join(self.file_path, "resources", "welcome_message.txt"), "r")
         self.cellar_f = open(os.path.join(self.file_path, "resources", "cellar.txt"), "r")
@@ -31,12 +33,15 @@ class Main:
         self.hidden_room_f = open(os.path.join(self.file_path, "resources", "hidden_room.txt"), "r")
         self.throne_room_f = open(os.path.join(self.file_path, "resources", "throne_room.txt"), "r")
 
+        # configure the name and author of the game
         RPGInfo.author = "Timo Früh"
         RPGInfo.title = "The Chosen"
         RPGInfo.subtitle = "At Nights End"
 
+        # set welcome message to the content of the text file loaded above
         RPGInfo.welcome_message = self.welcome_f.read()
 
+        # initialise all rooms and set their description to the corresponding text file
         self.cellar = Room(room_name="Cellar")
         self.cellar.set_desc(self.cellar_f.read())
 
@@ -76,6 +81,7 @@ class Main:
         self.throne_room = Room(room_name="The Throne Room")
         self.throne_room.set_desc(self.throne_room_f.read())
 
+        # connect all the rooms
         self.cellar.link_vertical(direction="up", room=self.cellar_ladder)
         self.cellar_ladder.link_vertical(direction="down", room=self.cellar)
 
@@ -114,6 +120,7 @@ class Main:
         self.throne_entrance.link_hidden(direction="south", room=self.hidden_room)
         self.hidden_room.link_hidden(direction="north", room=self.throne_entrance)
 
+        # initialise all items, set their (initial) description and their initial room
         self.longsword = Item(art="a", item_name="sword")
         self.longsword.set_description("a simple longsword, but it seems like good craftsmanship.")
         self.cellar.add_item(self.longsword)
@@ -136,13 +143,21 @@ class Main:
         self.water_bottle.set_description("standing on the ground.")
         self.library.add_item(self.water_bottle)
 
+        # print welcome message
         RPGInfo.welcome()
 
+        # prompt the player for a name
         self.player_name = input("What is your name? ")
+
+        # set player name to "Stranger" if none was provided
         if self.player_name.replace(" ", "") == "":
             self.player_name = "Stranger"
+
+        # initialise the player, set their name to the prevously configured player name
+        # and set their starting room to the cellar
         self.player = Player(player_name=self.player_name, starting_room=self.cellar)
 
+        # initialise all mobs and their weaknesses
         self.fire_demon = Mob(class_name="demon of fire")
         self.fire_demon.add_weakness(self.swords_odd)
         self.fire_demon.add_weakness(self.water_bottle)
@@ -183,6 +198,8 @@ class Main:
         self.air_demon2.add_weakness(self.crossbow)
         self.hall.add_character(self.air_demon2)
 
+        # initialise all characters, their descriptions, conversations, rooms and weaknesses
+        # and define whether they are able to kill the player
         self.elliot = Friend(character_name="Elliot")
         self.elliot.set_desc("a man you've never seen before. Or have you? How else would you know his name?")
         self.elliot.set_conversation(f"\tHey, {self.player_name}! Long time no see! Have you heard the latest gossip?\n"
@@ -233,74 +250,129 @@ class Main:
         self.demon_king.add_weakness(self.swords_odd)
         self.throne_room.add_character(self.demon_king)
 
+        # print "You look around." and a empty line after that
         print("\nYou look around.")
-
         print("")
 
+        # print the details of the current room
         self.player.get_current_room().describe()
 
+    # define the mainloop method, which is essentially where the magic happens
     def mainloop(self):
 
+        # set alive to true (it can later be set to false to end the game)
         alive = True
+
+        # set victory to false (it can later be set to true to end the game and display a victory message)
         victory = False
 
+        # repeat the program until alive becomes false or victory becomes true
         while alive and not victory:
+
+            # display a command prompt for the user
             print("")
             user_input = input("> ")
+
+            # set the command variable to the users input, make it all lowecase and remove spaces from end and beginning
             command = user_input.lower().strip()
 
+            # if the command is "commands" or "help" or "?" execute the print_commands() method from the Commands class
             if command in ["commands", "help", "?"]:
                 Cmd.print_commands()
 
+            # if the command is a direction execute the movement() method from the Commands class
             elif command in ["north", "east", "south", "west", "up", "down"]:
                 Cmd.movement(self.player, command)
 
+            # if the command is "look" or "l" execute the look() method from the Commands class
             elif command in ["look", "l"]:
                 Cmd.look(self.player)
 
+            # if "talk" is in the command do the following
             elif "talk" in command:
+
+                # interpret positional command "talk ..."
                 talk_input = InputInterpreter.interpret_single(command, "talk")
+
+                # execute the talk() method from the Commands class
                 Cmd.talk(self.player, talk_input)
 
+            # if the command is "inventory" or "i" or "backpack" execute the show_inventory() method from the Commands class
             elif command in ["inventory", "i", "backpack"]:
                 Cmd.show_inventory(self.player)
 
+            # if "fight" is in the command and the player is currently in the throne room do the following
             elif "fight" in command and self.player.get_current_room() == self.throne_room:
+
+                # interpret positional command "fight ... with ..."
                 boss_fight_input = InputInterpreter.interpret_double(command, "fight", "with")
+
+                # execute the fight() method from the Commands class and put its output into the boss_fight variable
                 boss_fight = Cmd.fight(self.player, who=boss_fight_input[0], item=boss_fight_input[1])
+
+                # set alive and victory according to the outcome of the fight
                 alive = boss_fight["alive"]
                 victory = boss_fight["victory"]
 
+            # if "fight" is in the command do the following
             elif "fight" in command:
+
+                # interpret positional command "fight ... with ...
                 fight_input = InputInterpreter.interpret_double(command, "fight", "with")
+
+                # execute the fight() method from the Commands class and use its output to set the alive variable
                 alive = Cmd.fight(self.player, who=fight_input[0], item=fight_input[1])["alive"]
 
+            # if "take" is in the command do the following
             elif "take" in command:
+
+                # interpret positional command "talk ..."
                 take_input = InputInterpreter.interpret_single(command, "take")
+
+                # execute the take() method from the Commands class
                 Cmd.take(self.player, take_input)
 
+            # if "drop" is in the command do the following
             elif "drop" in command:
+
+                # interpret positional command "drop ..."
                 drop_input = InputInterpreter.interpret_single(command, "drop")
+
+                # execute the drop() method from the Commands class
                 Cmd.drop(self.player, drop_input)
 
+            # if "hug" is in the command do the following
             elif "hug" in command:
+
+                # interpret the positional command "hug ..."
                 hug_input = InputInterpreter.interpret_single(command, "hug")
+
+                # execute the hug() method from the Commands class
                 Cmd.hug(self.player, hug_input)
 
+            # if the command is "quit" or "exit"
             elif command in ["quit", "exit"]:
+
+                # execute the quit() method from the Commands class and set its output to the confirm variable
                 confirm = Cmd.quit()
+
+                # end the game if the output was "y"
                 if confirm == "y":
                     alive = False
 
+            # if the command is empty pass on
             elif command == "":
                 pass
 
+            # if the command was none of the above print an error message
             else:
                 print(f"I do not know what you meant by {user_input}.")
 
+        # print a victory message if victory is true after the end of the loop
         if victory:
             print("\nCongratulations! You have been victorious and thereby beaten the game!\n")
 
+        # print kill message (depending on how many the user scored)
         if self.player.get_kills() == 0:
             print(f"You vanquished not a single enemy during the game.")
         elif self.player.get_kills() == 1:
@@ -308,19 +380,29 @@ class Main:
         elif self.player.get_kills() > 1:
             print(f"You vanquished {self.player.get_kills()} enemies during the game.")
 
-        print("")
-        RPGInfo.credits()
+        # print an empty line after the kill message
         print("")
 
+        # print the credits
+        RPGInfo.credits()
+
+        # print an empty line after the credits
+        print("")
+
+        # print a message to hit enter to exit the game
         input("[Hit enter to exit.]")
+
+        # clear the screen again after the end of the game
         clear()
 
 
+# define a main() function in which the Main class and its mainloop() method are called
 def main():
     game = Main()
 
     game.mainloop()
 
 
+# run the main() function if the program was started from this file
 if __name__ == "__main__":
     main()
