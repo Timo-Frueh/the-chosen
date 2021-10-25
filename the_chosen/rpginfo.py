82654a2b2688763f26463bcb8c7e1c7d65d5c07c
Timo Früh <timo.frueh@icloud.com>
2021-10-25 20:59:18 +0200

    add comments to rpginfo.py

--- the_chosen/rpginfo.py
+++ the_chosen/rpginfo.py
@@ -2,17 +2,21 @@
 # Full copyright notice in main.py
 
 class RPGInfo:
+
+    # define the class variables
     author = None
     title = None
     subtitle = None
     welcome_message = None
 
+    # print a welcome message
     @classmethod
     def welcome(cls):
         print(f"Welcome to {cls.title}: {cls.subtitle}")
         print("")
         print(cls.welcome_message)
 
+    # print credits
     @classmethod
     def credits(cls):
         print(f"This was {cls.title}.")
