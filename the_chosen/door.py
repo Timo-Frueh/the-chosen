4dab63d6a5bfc10554e91e6582ff5f0b54383598
Timo Früh <timo.frueh@icloud.com>
2021-11-23 10:49:43 +0100

    add link class and ladder class

--- the_chosen/door.py
+++ the_chosen/door.py
@@ -1,8 +1,31 @@
-class Door:
-    def __init__(self, isopen, con_from, con_to):
+from os import CLD_CONTINUED
+
+
+class Link:
+    def __init__(self, connect_1, connect_2):
+        self.connections = [connect_1, connect_2]
+
+    def get_connections(self):
+        return self.connections
+
+
+class Door(Link):
+    def __init__(self, isopen, connect_1, connect_2):
+        super().__init__(connect_1, connect_2)
         self.open = isopen
-        self.connect_from = con_from
-        self.connect_to = con_to
     
-    def pass_through(self, current_room):
-        pass
+    def open(self):
+        self.open = True
+        print("You opened the door.")
+
+    def close(self):
+        self.open = False
+        print("You closed the door.")
+    
+    def isopen(self):
+        return self.open
+
+
+class Ladder(Link):
+    def __init__(self, connect_1, connect_2):
+        super().__init__(connect_1, connect_2)
