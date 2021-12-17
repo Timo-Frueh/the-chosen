129995748ecb9a24c8815e2235ea7ec133a8357f
Timo Früh <timo.frueh@icloud.com>
2021-12-17 12:26:43 +0100

    change version in setup.py to dev

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='the-chosen',
-    version='1.0.5',
+    version='dev',
     packages=['the_chosen'],
     url='https://github.com/Timo-Frueh/the-chosen',
     license='GPL-3.0',
