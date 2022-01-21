dae985cb3220c32e550d2f3e21188d014fbc600b
Timo Früh <timo.frueh@icloud.com>
2022-01-21 10:54:09 +0100

    fix documentation in rpginfo.py

--- the_chosen/rpginfo.py
+++ the_chosen/rpginfo.py
@@ -1,26 +1,37 @@
 # coding=utf-8
 
+"""
+This module holds the RPGInfo class.
+"""
+
 # The Chosen  Copyright (C) 2021  Timo Früh
 # Full copyright notice in main.py
 
+
 class RPGInfo:
 
-    # define the class variables
+    # define some class variables
     author = None
     title = None
     subtitle = None
     welcome_message = None
 
-    # print a welcome message
     @classmethod
     def welcome(cls):
+        """
+        Print a welcome message.
+        """
+
         print(f"Welcome to {cls.title}: {cls.subtitle}")
         print("")
         print(cls.welcome_message)
 
-    # print credits
     @classmethod
     def credits(cls):
+        """
+        Print credits.
+        """
+        
         print(f"This was {cls.title}.")
         print("Thank you for playing!")
         print(f"\nMade by {cls.author}.")
