54b412ba14e749466f7a20c64627aa2a10d87bf3
Timo Früh <timo.frueh@icloud.com>
2022-04-22 11:57:29 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -4,12 +4,12 @@ setup(
     name='the-chosen',
     version='dev',
     packages=['the_chosen'],
-    url='https://github.com/Timo-Frueh/the-chosen',
+    url='https://github.com/tifrueh/the-chosen',
     license='GPL-3.0',
-    author='tifrueh',
+    author='Timo Früh',
     author_email='timo.frueh@icloud.com',
     description='A short text-adventure',
     install_requires=['clear-screen~=0.1.14'],
     package_data={'the_chosen': ['resources/*']},
-    entry_points={'console_scripts': ['the-chosen=the_chosen.main:main']}
+    entry_points={'console_scripts': ['the-chosen=the_chosen.__main__:main']}
 )
